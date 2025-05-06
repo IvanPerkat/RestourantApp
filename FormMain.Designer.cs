48eb1074308116055fa03d4635d5e007b31006c5
Peki <ivan.perkat06@gmail.com>
2025-05-06 20:01:26 +0200

    Added function which generates item panels

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -35,40 +35,15 @@
             label2 = new Label();
             label1 = new Label();
             pictureBoxLogo = new PictureBox();
-            panelMain = new Panel();
-            panel2 = new Panel();
-            label7 = new Label();
-            label8 = new Label();
-            panel1 = new Panel();
-            label6 = new Label();
-            label5 = new Label();
             panelOrder = new Panel();
             labelTotalPriceNumber = new Label();
             labelTotalPrice = new Label();
             buttonOrder = new Button();
             labelOrder = new Label();
-            panel3 = new Panel();
-            label9 = new Label();
-            label10 = new Label();
-            panel4 = new Panel();
-            label11 = new Label();
-            label12 = new Label();
-            panel5 = new Panel();
-            label13 = new Label();
-            label14 = new Label();
-            panel6 = new Panel();
-            label15 = new Label();
-            label16 = new Label();
+            flowLayoutPanelMain = new FlowLayoutPanel();
             panelNavigation.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBoxLogo).BeginInit();
-            panelMain.SuspendLayout();
-            panel2.SuspendLayout();
-            panel1.SuspendLayout();
             panelOrder.SuspendLayout();
-            panel3.SuspendLayout();
-            panel4.SuspendLayout();
-            panel5.SuspendLayout();
-            panel6.SuspendLayout();
             SuspendLayout();
             // 
             // panelNavigation
@@ -82,7 +57,7 @@
             panelNavigation.Dock = DockStyle.Left;
             panelNavigation.Location = new Point(0, 0);
             panelNavigation.Name = "panelNavigation";
-            panelNavigation.Size = new Size(200, 515);
+            panelNavigation.Size = new Size(200, 611);
             panelNavigation.TabIndex = 1;
             // 
             // label4
@@ -139,88 +114,6 @@
             pictureBoxLogo.TabIndex = 1;
             pictureBoxLogo.TabStop = false;
             // 
-            // panelMain
-            // 
-            panelMain.AutoScroll = true;
-            panelMain.BackColor = Color.FromArgb(24, 24, 24);
-            panelMain.Controls.Add(panel6);
-            panelMain.Controls.Add(panel5);
-            panelMain.Controls.Add(panel4);
-            panelMain.Controls.Add(panel3);
-            panelMain.Controls.Add(panel2);
-            panelMain.Controls.Add(panel1);
-            panelMain.Controls.Add(panelOrder);
-            panelMain.Dock = DockStyle.Fill;
-            panelMain.Font = new Font("Microsoft YaHei UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            panelMain.Location = new Point(200, 0);
-            panelMain.Name = "panelMain";
-            panelMain.Size = new Size(1024, 515);
-            panelMain.TabIndex = 2;
-            // 
-            // panel2
-            // 
-            panel2.BackColor = Color.FromArgb(248, 248, 248);
-            panel2.Controls.Add(label7);
-            panel2.Controls.Add(label8);
-            panel2.Location = new Point(27, 250);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(225, 196);
-            panel2.TabIndex = 2;
-            // 
-            // label7
-            // 
-            label7.AutoSize = true;
-            label7.BackColor = Color.FromArgb(0, 168, 107);
-            label7.FlatStyle = FlatStyle.Popup;
-            label7.Font = new Font("Microsoft YaHei UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            label7.Location = new Point(156, 145);
-            label7.Name = "label7";
-            label7.Size = new Size(51, 28);
-            label7.TabIndex = 1;
-            label7.Text = "25€";
-            // 
-            // label8
-            // 
-            label8.AutoSize = true;
-            label8.Font = new Font("Microsoft YaHei UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            label8.Location = new Point(13, 147);
-            label8.Name = "label8";
-            label8.Size = new Size(123, 19);
-            label8.TabIndex = 0;
-            label8.Text = "ISTARSKA PLATA";
-            // 
-            // panel1
-            // 
-            panel1.BackColor = Color.FromArgb(248, 248, 248);
-            panel1.Controls.Add(label6);
-            panel1.Controls.Add(label5);
-            panel1.Location = new Point(27, 28);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(225, 196);
-            panel1.TabIndex = 1;
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.BackColor = Color.FromArgb(0, 168, 107);
-            label6.FlatStyle = FlatStyle.Popup;
-            label6.Font = new Font("Microsoft YaHei UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            label6.Location = new Point(156, 145);
-            label6.Name = "label6";
-            label6.Size = new Size(51, 28);
-            label6.TabIndex = 1;
-            label6.Text = "25€";
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Font = new Font("Microsoft YaHei UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            label5.Location = new Point(13, 147);
-            label5.Name = "label5";
-            label5.Size = new Size(123, 19);
-            label5.TabIndex = 0;
-            label5.Text = "ISTARSKA PLATA";
-            // 
             // panelOrder
             // 
             panelOrder.BackColor = Color.FromArgb(18, 18, 18);
@@ -230,11 +123,11 @@
             panelOrder.Controls.Add(labelOrder);
             panelOrder.Dock = DockStyle.Right;
             panelOrder.Font = new Font("Microsoft JhengHei UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            panelOrder.Location = new Point(786, 0);
+            panelOrder.Location = new Point(937, 0);
             panelOrder.Name = "panelOrder";
             panelOrder.RightToLeft = RightToLeft.No;
-            panelOrder.Size = new Size(238, 515);
-            panelOrder.TabIndex = 0;
+            panelOrder.Size = new Size(237, 611);
+            panelOrder.TabIndex = 3;
             // 
             // labelTotalPriceNumber
             // 
@@ -253,7 +146,7 @@
             labelTotalPrice.AutoSize = true;
             labelTotalPrice.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 238);
             labelTotalPrice.ForeColor = Color.FromArgb(0, 168, 107);
-            labelTotalPrice.Location = new Point(18, 441);
+            labelTotalPrice.Location = new Point(18, 537);
             labelTotalPrice.Name = "labelTotalPrice";
             labelTotalPrice.Size = new Size(75, 21);
             labelTotalPrice.TabIndex = 1;
@@ -262,13 +155,12 @@
             // buttonOrder
             // 
             buttonOrder.BackColor = Color.FromArgb(0, 168, 107);
-            buttonOrder.Dock = DockStyle.Bottom;
             buttonOrder.FlatAppearance.BorderColor = Color.FromArgb(18, 18, 18);
             buttonOrder.FlatAppearance.BorderSize = 0;
             buttonOrder.FlatStyle = FlatStyle.Flat;
             buttonOrder.Font = new Font("Microsoft YaHei UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 238);
             buttonOrder.ForeColor = Color.FromArgb(248, 248, 248);
-            buttonOrder.Location = new Point(0, 475);
+            buttonOrder.Location = new Point(0, 571);
             buttonOrder.Name = "buttonOrder";
             buttonOrder.Size = new Size(238, 40);
             buttonOrder.TabIndex = 1;
@@ -286,196 +178,48 @@
             labelOrder.TabIndex = 0;
             labelOrder.Text = "Narudžba";
             // 
-            // panel3
+            // flowLayoutPanelMain
             // 
-            panel3.BackColor = Color.FromArgb(248, 248, 248);
-            panel3.Controls.Add(label9);
-            panel3.Controls.Add(label10);
-            panel3.Location = new Point(281, 28);
-            panel3.Name = "panel3";
-            panel3.Size = new Size(225, 196);
-            panel3.TabIndex = 2;
-            // 
-            // label9
-            // 
-            label9.AutoSize = true;
-            label9.BackColor = Color.FromArgb(0, 168, 107);
-            label9.FlatStyle = FlatStyle.Popup;
-            label9.Font = new Font("Microsoft YaHei UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            label9.Location = new Point(156, 145);
-            label9.Name = "label9";
-            label9.Size = new Size(51, 28);
-            label9.TabIndex = 1;
-            label9.Text = "25€";
-            // 
-            // label10
-            // 
-            label10.AutoSize = true;
-            label10.Font = new Font("Microsoft YaHei UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            label10.Location = new Point(13, 147);
-            label10.Name = "label10";
-            label10.Size = new Size(123, 19);
-            label10.TabIndex = 0;
-            label10.Text = "ISTARSKA PLATA";
-            // 
-            // panel4
-            // 
-            panel4.BackColor = Color.FromArgb(248, 248, 248);
-            panel4.Controls.Add(label11);
-            panel4.Controls.Add(label12);
-            panel4.Location = new Point(281, 250);
-            panel4.Name = "panel4";
-            panel4.Size = new Size(225, 196);
-            panel4.TabIndex = 2;
-            // 
-            // label11
-            // 
-            label11.AutoSize = true;
-            label11.BackColor = Color.FromArgb(0, 168, 107);
-            label11.FlatStyle = FlatStyle.Popup;
-            label11.Font = new Font("Microsoft YaHei UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            label11.Location = new Point(156, 145);
-            label11.Name = "label11";
-            label11.Size = new Size(51, 28);
-            label11.TabIndex = 1;
-            label11.Text = "25€";
-            // 
-            // label12
-            // 
-            label12.AutoSize = true;
-            label12.Font = new Font("Microsoft YaHei UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            label12.Location = new Point(13, 147);
-            label12.Name = "label12";
-            label12.Size = new Size(123, 19);
-            label12.TabIndex = 0;
-            label12.Text = "ISTARSKA PLATA";
-            // 
-            // panel5
-            // 
-            panel5.BackColor = Color.FromArgb(248, 248, 248);
-            panel5.Controls.Add(label13);
-            panel5.Controls.Add(label14);
-            panel5.Location = new Point(536, 28);
-            panel5.Name = "panel5";
-            panel5.Size = new Size(225, 196);
-            panel5.TabIndex = 2;
-            // 
-            // label13
-            // 
-            label13.AutoSize = true;
-            label13.BackColor = Color.FromArgb(0, 168, 107);
-            label13.FlatStyle = FlatStyle.Popup;
-            label13.Font = new Font("Microsoft YaHei UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            label13.Location = new Point(156, 145);
-            label13.Name = "label13";
-            label13.Size = new Size(51, 28);
-            label13.TabIndex = 1;
-            label13.Text = "25€";
-            // 
-            // label14
-            // 
-            label14.AutoSize = true;
-            label14.Font = new Font("Microsoft YaHei UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            label14.Location = new Point(13, 147);
-            label14.Name = "label14";
-            label14.Size = new Size(123, 19);
-            label14.TabIndex = 0;
-            label14.Text = "ISTARSKA PLATA";
-            // 
-            // panel6
-            // 
-            panel6.BackColor = Color.FromArgb(248, 248, 248);
-            panel6.Controls.Add(label15);
-            panel6.Controls.Add(label16);
-            panel6.Location = new Point(536, 250);
-            panel6.Name = "panel6";
-            panel6.Size = new Size(225, 196);
-            panel6.TabIndex = 2;
-            // 
-            // label15
-            // 
-            label15.AutoSize = true;
-            label15.BackColor = Color.FromArgb(0, 168, 107);
-            label15.FlatStyle = FlatStyle.Popup;
-            label15.Font = new Font("Microsoft YaHei UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            label15.Location = new Point(156, 145);
-            label15.Name = "label15";
-            label15.Size = new Size(51, 28);
-            label15.TabIndex = 1;
-            label15.Text = "25€";
-            // 
-            // label16
-            // 
-            label16.AutoSize = true;
-            label16.Font = new Font("Microsoft YaHei UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            label16.Location = new Point(13, 147);
-            label16.Name = "label16";
-            label16.Size = new Size(123, 19);
-            label16.TabIndex = 0;
-            label16.Text = "ISTARSKA PLATA";
+            flowLayoutPanelMain.BackColor = Color.FromArgb(28, 28, 28);
+            flowLayoutPanelMain.Dock = DockStyle.Fill;
+            flowLayoutPanelMain.Location = new Point(200, 0);
+            flowLayoutPanelMain.Name = "flowLayoutPanelMain";
+            flowLayoutPanelMain.Size = new Size(737, 611);
+            flowLayoutPanelMain.TabIndex = 4;
             // 
             // FormMain
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1224, 515);
-            Controls.Add(panelMain);
+            ClientSize = new Size(1174, 611);
+            Controls.Add(flowLayoutPanelMain);
+            Controls.Add(panelOrder);
             Controls.Add(panelNavigation);
             Font = new Font("Microsoft JhengHei UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
             MaximizeBox = false;
             Name = "FormMain";
             Text = "Restourant";
+            Load += FormMain_Load;
             panelNavigation.ResumeLayout(false);
             panelNavigation.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBoxLogo).EndInit();
-            panelMain.ResumeLayout(false);
-            panel2.ResumeLayout(false);
-            panel2.PerformLayout();
-            panel1.ResumeLayout(false);
-            panel1.PerformLayout();
             panelOrder.ResumeLayout(false);
             panelOrder.PerformLayout();
-            panel3.ResumeLayout(false);
-            panel3.PerformLayout();
-            panel4.ResumeLayout(false);
-            panel4.PerformLayout();
-            panel5.ResumeLayout(false);
-            panel5.PerformLayout();
-            panel6.ResumeLayout(false);
-            panel6.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
         private Panel panelNavigation;
-        private Panel panelMain;
-        private Panel panel6;
-        private Panel panel5;
         private PictureBox pictureBoxLogo;
-        private Panel panelOrder;
-        private Label labelOrder;
-        private Button buttonOrder;
-        private Label labelTotalPriceNumber;
-        private Label labelTotalPrice;
         private Label label3;
         private Label label2;
         private Label label1;
         private Label label4;
-        private Panel panel1;
-        private Label label5;
-        private Label label6;
-        private Panel panel2;
-        private Label label7;
-        private Label label8;
-        private Label label15;
-        private Label label16;
-        private Label label13;
-        private Label label14;
-        private Panel panel4;
-        private Label label11;
-        private Label label12;
-        private Panel panel3;
-        private Label label9;
-        private Label label10;
+        private Panel panelOrder;
+        private Label labelTotalPriceNumber;
+        private Label labelTotalPrice;
+        private Button buttonOrder;
+        private Label labelOrder;
+        private FlowLayoutPanel flowLayoutPanelMain;
     }
 }
--- FormMain.cs
+++ FormMain.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace RestourantApp
 {
     public partial class FormMain : Form
@@ -5,6 +7,50 @@ namespace RestourantApp
         public FormMain()
         {
             InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            List<Article> articles = new List<Article>
+            {
+                new Article { Name = "ISTARSKA PLATA", Price = "10€" },
+                new Article { Name = "VALENTINOVA MUDA", Price = "0€" },
+                new Article { Name = "MARKOVA MUDA", Price = "100€" }
+            };
+
+            Panel Item(string name, string price)
+            {
+                Panel item = new Panel();
+                item.Size = new Size(225, 200);
+                item.BackColor = Color.FromArgb(248, 248, 248);
+                item.Margin = new Padding(10);
+
+                Label labelName = new Label();
+                labelName.Text = name;
+                labelName.TextAlign = ContentAlignment.MiddleLeft;
+                labelName.Size = new Size(200, 20);
+                labelName.Font = new Font("Microsoft YaHei UI", 10, FontStyle.Bold);
+                labelName.Location = new Point(15, 135);
+
+                Label labelPrice = new Label();
+                labelPrice.Text = price;
+                labelPrice.TextAlign = ContentAlignment.MiddleCenter;
+                labelPrice.Size = new Size(70, 30);
+                labelPrice.Font = new Font("Microsoft YaHei UI", 16, FontStyle.Bold);
+                labelPrice.Location = new Point(18, 160);
+                labelPrice.BackColor = Color.FromArgb(0, 168, 107);
+
+                item.Controls.Add(labelName);
+                item.Controls.Add(labelPrice);
+
+                return item;
+            }
+
+            foreach (var article in articles)
+            {
+                var item = Item(article.Name, article.Price);
+                flowLayoutPanelMain.Controls.Add(item);
+            }
         }
     }
 }
